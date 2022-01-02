0e363a8ae297ae94b3abe4e5249ac6f3975b16ad
ThanhLe054 <thanhle.eiq@gmail.com>
2022-01-02 16:23:36 +0700

    da chuyen file moi vao

---
+++ main.rs
@@ -0,0 +1,68 @@
+//mod Structs;
+//mod tuplesstructs;
+mod Ownership;
+//mod referencess;
+//mod pattern_matching;
+//mod reading_user_input;
+//mod hash_maps;
+fn main() {
+    
+//pattern_matching::p_matching();
+//reading_user_input::reading_user_input();
+//hash_maps::hash_map();
+
+
+
+
+
+
+
+
+
+//let mut s = String::from("hello");
+//let _r1 = &mut s;
+//let _r2=&mut s;
+//println!("{}",_r1);
+
+//referencess::okmain();
+Ownership::Owner();
+
+
+
+// fn _x(){
+//     let mut _a ="hello" ;          //.to_string();
+//     let _b = 100;
+//    // _a.push_str("!");
+//     _y();
+// }
+
+// fn _y(){
+//     let mut _a = String::from("World");
+//     _a.push_str("!");
+// }
+
+
+
+    //Structs::run();
+
+
+//     let x = plus_one(5);
+//     println!("Value {}",x);
+// }
+
+// fn plus_one(mut y:i32)->i32{
+
+//     y+=1;
+//     y+1;
+//     return y
+
+// {
+
+//     let y=5;
+//     println!("Value {}",y);
+//     let y =6;
+//     println!("Value {}",y);
+
+// }
+    
+}
